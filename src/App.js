d37f73993f3ef686b52373a586247ced66ec6224
Steven Wu <steven.wu@flinders.edu.au>
2017-03-08 16:03:10 +1030

    Refactor to move state and methods to App component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Form from './Form';
 
 class App extends Component {
 
-  render() {
-    return (
-        <div>
-            <Form/>
-        </div>
-    )
-  }
+    state = {
+        name: '',
+        email: ''
+    }
+
+    componentDidMount() {
+        let name = localStorage.getItem('name');
+        let email = localStorage.getItem('email');
+
+        this.setState({
+            name: name ? name : '',
+            email: email ? email : ''
+        })
+    }
+
+    handleNameChange(e) {
+        let newValue = e.target.value
+        localStorage.setItem('name', newValue)
+
+        this.setState({
+            name: newValue
+        })
+    }
+
+    handleEmailChange(e) {
+        let newValue = e.target.value
+        localStorage.setItem('email', newValue)
+        
+        this.setState({
+            email: newValue
+        })
+    }
+
+    clear() {
+        localStorage.clear()
+        this.setState({
+            name: '',
+            email: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Form name={this.state.name}
+                      email={this.state.email}
+                      onNameChange={e => this.handleNameChange(e)}
+                      onEmailChange={e => this.handleEmailChange(e)}
+                      onClear={() => this.clear()}/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -3,53 +3,20 @@ import './Form.css';
 
 class Form extends Component {
 
-    static state = {
-        name: '',
-        email: ''
-    }
-
-    handleNameChange(e) {
-        let newValue = e.target.value
-        localStorage.setItem('name', newValue)
-        this.setState({
-            name: newValue
-        })
-    }
-
-    handleEmailChange(e) {
-        let newValue = e.target.value
-        localStorage.setItem('email', newValue)
-        this.setState({
-            email: newValue
-        })
-    }
-
-    clear() {
-        localStorage.clear()
-        this.setState({
-            name: '',
-            email: ''
-        })
-    }
-
-    loadData(key) {
-        return localStorage.getItem(key)
-    }
-
     render() {
         return (
             <form action="/my-handling-form-page" method="post">
                 <div>
                     <label>Name:</label>
-                    <input type="text" id="name" name="user_name" value={this.loadData('name') ? this.loadData('name') : ''} onChange={e => this.handleNameChange(e)}/>
+                    <input type="text" id="name" value={this.props.name} onChange={this.props.onNameChange}/>
                 </div>
                 <div>
                     <label>E-mail:</label>
-                    <input type="email" id="mail" name="user_mail" value={this.loadData('email') ? this.loadData('email') : ''} onChange={e => this.handleEmailChange(e)}/>
+                    <input type="email" id="mail" value={this.props.email} onChange={this.props.onEmailChange}/>
                 </div>
                 <div className="button">
                     <button type="submit">Send your message</button>
-                    <button onClick={() => this.clear()}>Clear</button>
+                    <button onClick={this.props.onClear}>Clear</button>
                 </div>
             </form>
         )
